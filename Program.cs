a7b33185aefb4b6d428f997b00a1420e82dde939
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-28 14:19:48 +0300

    lesson 4

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿int [] CreateArray (int min, int max, int size){
+
+    int [] array = new int [size];
+    for (int i = 0; i < size; i ++){
+        array [i] = new Random ().Next(min,max+1);
+    }
+    return array;
+}
+
+void ShowArray (int [] array){
+    for (int i = 0; i<array.Length; i++){
+        Console.Write (array [i]+ " ");
+    }
+    Console.WriteLine ();
+}
+bool FindDigit (int [] array, int digit) {
+    bool flag = false;
+    for (int i = 0; i<array.Length; i++){
+        if (array [i]==digit){
+            return true;
+        }
+    }
+    return false;
+}
+
+Console.WriteLine ("Enter array size");
+int size = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Enter array min");
+int min = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Enter array max");
+int max = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Enter search digit");
+int digit = Convert.ToInt32 (Console.ReadLine());
+
+int [] array = CreateArray (min,max,size);
+ShowArray(array);
+bool res = FindDigit (array, digit);
+if (res == true){
+    Console.WriteLine ("Да");
+
+}
+else {
+    Console.WriteLine ("Нет");
+}
